df264732868eb32476702665b7160df6cc22a202
James Tease <james@jamestease.co.uk>
2016-09-24 18:32:30 +0100

    tests

--- tests/components/Header/Header.spec.js
+++ tests/components/Header/Header.spec.js
@@ -1,38 +1,43 @@
 import React from 'react';
-import { Header } from 'components/Header/Header';
+import Header from 'components/Header/Header';
 import { IndexLink, Link } from 'react-router';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 describe('(Component) Header', () => {
   let _wrapper;
 
   beforeEach(() => {
-    _wrapper = shallow(<Header />);
+    _wrapper = mount(<Header />);
   });
 
+  describe('toggle', () => {
+    it('should have a toggle', () => {
+      expect(_wrapper.find('.nav__toggle')).to.exist;
+    });
+
+    it('should toggle nav visibility', () => {
+      _wrapper.find('.nav__toggle').simulate('click');
+
+      expect(_wrapper.state('visible')).to.equal(true);
+      expect(_wrapper.find('.nav')).to.exist;
+    });
+  })
   describe('Navigation links...', () => {
-    it('Should render a Link to Home route', () => {
-      expect(_wrapper.contains(
-        <IndexLink activeClassName='route--active' to='/'>
-          Home
-        </IndexLink>
-      )).to.be.true;
+    beforeEach(() => {
+      _wrapper.setState({ visible: true });
     });
 
     it('Should render a Link to Letter route', () => {
-      expect(_wrapper.contains(
-        <Link activeClassName='route--active' to='/letter'>
-          Letter
-        </Link>
-      )).to.be.true;
+      expect(_wrapper.find(IndexLink)).to.exist;
+      expect(_wrapper.find(IndexLink).text()).to.match(/Letter/);
+      expect(_wrapper.find(IndexLink).prop('to')).to.equal('/');
     });
 
     it('Should render a Link to Lottery route', () => {
-      expect(_wrapper.contains(
-        <Link activeClassName='route--active' to='/lottery'>
-          Lottery
-        </Link>
-      )).to.be.true;
+      expect(_wrapper.find(Link)).to.exist;
+      expect(_wrapper.find(Link).at(1).text()).to.match(/Lottery/);
+
+      expect(_wrapper.find(Link).at(1).prop('to')).to.equal('/lottery');
     });
   });
 });
