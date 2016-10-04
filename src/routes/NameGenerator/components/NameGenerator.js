ce3178fc2d7f301fcc4df2b112550cc5c3356a5e
James Tease <james@jamestease.co.uk>
2016-10-04 15:58:07 +0100

    Tidying

--- src/routes/NameGenerator/components/NameGenerator.js
+++ src/routes/NameGenerator/components/NameGenerator.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { getRandomInt } from 'services/utils';
 
-const prefixes = ["Asylum", "Bell", "Black", "Capra", "Ceaseless", "Centipede", "Chaos", "Witch", "Dragon", "Crossbreed", "Dark Sun", "Demon", "Slayer", "Executioner", "Gaping", "Gravelord", "Lord", "Iron", "Cinder", "Knight", "Father", "Abyss", "Moonlight", "Sanctuary", "Stray", "Taurus", "Armoured", "Golden", "Crystal", "Giant", "Undead", "Giant Undead", "Hellkite", "Parasitic", "Prowling", "Prince", "Grey", "Maneater", "Iron", "Paladin", "Xanthous", "Marvellous", "Big Hat", "Black Iron", "Crestfallen", "Darkstalker", "Gravelord", "Hawkeye", "Kingseeker", "Lord's Blade", "Stone", "Silent", "Belfry", "Captain", "Emerald", "Grave Warden", "Lonesome", "Manscorpion", "Hag", "Mild Mannered", "Royal", "Sorcerer", "Sparkling", "Steady Hand", "Old", "Ruin", "Old Iron", "Covetous", "Baleful", "Prowling", "Ancient", "Burnt", "Slumbering", "Ivory", "Fume", "Sir", "Nameless", "Pilgrim", "Jester", "Ashen", "Abbess", "Rapacious", "Drifter", "Woodland Child", "Peculiar", "Holy", "Yellowfinger", "Longfinger", "Knight-Slayer", "Curse-Rotted", "Deacon", "High Lord", "Old Demon", "Pontiff", "Boreal", "Unbreakable", "Ringfinger"];
-const types = ["Demon", "Gargoyle", "Dragon", "Witch", "Golem", "Knight", "Wolf", "Butcher", "Tusk", "Golem", "Rat", "Hydra", "Wall Hugger", "Prince", "Slayer", "King", "Blacksmith", "Princess", "Merchant", "Scholar", "Oracle", "Guard", "Captain", "Chancellor", "Herald", "Housekeeper", "Laddersmith", "Manscorpion", "Warrior", "Trader", "Lord", "Sentinel", "Queen", "Ogre", "Denizen", "Seeker", "Watcher", "Devourer", "Outrider Knight", "High Priestess", "Mother"];
-const suffixes = ["of Chaos", "of the Abyss", "of Cinder", "of Thorns", "of the Darkroot Wood", "of Astora", "of Zena", "of Oolacile", "of Vinheim", "of Sunlight", "of Carim", "of the Great Swamp", "of Thorolund", "of Izalith", "of the East", "of Catarina", "of the First Sin", "of Jugo", "of Mirrah", "of Lanafir", "of Olaphis", "of Song", "of Londor", "of the Spurned", "of the Sunless Realms", "of Carim", "of the Boreal Valley", "of the Deep"];
-const nicknames = ["the Scaleless", "the Great", "the Rock", "the Crow", "the Cartographer", "the Wanderer", "the Pardoner", "the Outcast", "the Armourer", "the Crestfallen", "the Lost", "the Ruined", "the Baleful", "the King's Pet", "the Squalid", "the Explorer", "the Butcher", "the Deserter", "the Hushed", "the Giant", "the Consumed", "of Lothric Castle", "of Courland", "of Rebirth"];
+/* eslint-disable max-len */
+const prefixes = ['Asylum', 'Bell', 'Black', 'Capra', 'Ceaseless', 'Centipede', 'Chaos', 'Crossbreed', 'Dark Sun', 'Slayer', 'Executioner', 'Gaping', 'Gravelord', 'Iron', 'Cinder', 'Father', 'Abyss', 'Moonlight', 'Sanctuary', 'Stray', 'Taurus', 'Armoured', 'Golden', 'Crystal', 'Giant', 'Undead', 'Giant Undead', 'Hellkite', 'Parasitic', 'Prowling', 'Prince', 'Grey', 'Maneater', 'Iron', 'Paladin', 'Xanthous', 'Marvellous', 'Big Hat', 'Black Iron', 'Crestfallen', 'Darkstalker', 'Gravelord', 'Hawkeye', 'Kingseeker', 'Lord\'s Blade', 'Stone', 'Silent', 'Belfry', 'Captain', 'Emerald', 'Grave Warden', 'Lonesome', 'Manscorpion', 'Hag', 'Mild Mannered', 'Royal', 'Sorcerer', 'Sparkling', 'Steady Hand', 'Old', 'Ruin', 'Old Iron', 'Covetous', 'Baleful', 'Prowling', 'Ancient', 'Burnt', 'Slumbering', 'Ivory', 'Fume', 'Sir', 'Nameless', 'Pilgrim', 'Jester', 'Ashen', 'Abbess', 'Rapacious', 'Drifter', 'Woodland Child', 'Peculiar', 'Holy', 'Yellowfinger', 'Longfinger', 'Knight-Slayer', 'Curse-Rotted', 'Deacon', 'High Lord', 'Old Demon', 'Pontiff', 'Boreal', 'Unbreakable', 'Ringfinger'];
+const types = ['Demon', 'Gargoyle', 'Dragon', 'Witch', 'Golem', 'Knight', 'Wolf', 'Butcher', 'Tusk', 'Golem', 'Rat', 'Hydra', 'Wall Hugger', 'Prince', 'Slayer', 'King', 'Blacksmith', 'Princess', 'Merchant', 'Scholar', 'Oracle', 'Guard', 'Captain', 'Chancellor', 'Herald', 'Housekeeper', 'Laddersmith', 'Manscorpion', 'Warrior', 'Trader', 'Lord', 'Sentinel', 'Queen', 'Ogre', 'Denizen', 'Seeker', 'Watcher', 'Devourer', 'Outrider Knight', 'High Priestess', 'Mother'];
+const suffixes = ['of Chaos', 'of the Abyss', 'of Cinder', 'of Thorns', 'of the Darkroot Wood', 'of Astora', 'of Zena', 'of Oolacile', 'of Vinheim', 'of Sunlight', 'of Carim', 'of the Great Swamp', 'of Thorolund', 'of Izalith', 'of the East', 'of Catarina', 'of the First Sin', 'of Jugo', 'of Mirrah', 'of Lanafir', 'of Olaphis', 'of Song', 'of Londor', 'of the Spurned', 'of the Sunless Realms', 'of Carim', 'of the Boreal Valley', 'of the Deep', 'of Lothric Castle', 'of Courland', 'of Rebirth'];
+const nicknames = ['the Scaleless', 'the Great', 'the Rock', 'the Crow', 'the Cartographer', 'the Wanderer', 'the Pardoner', 'the Outcast', 'the Armourer', 'the Crestfallen', 'the Lost', 'the Ruined', 'the Baleful', 'the King\'s Pet', 'the Squalid', 'the Explorer', 'the Butcher', 'the Deserter', 'the Hushed', 'the Giant', 'the Consumed'];
+/* eslint-enable max-len */
 
 export default class NameGenerator extends React.Component {
   state= {
@@ -15,7 +17,8 @@ export default class NameGenerator extends React.Component {
     let out;
     const name = 'Brian';
 
-    switch (getRandomInt(1, 9)) {
+    switch (getRandomInt(1, 10)) {
+      /* eslint-disable max-len */
       case 1:
       // prefix type
         out = `${ prefixes[getRandomInt(0, prefixes.length - 1)] } ${ types[getRandomInt(0, types.length - 1)] }`;
@@ -26,6 +29,11 @@ export default class NameGenerator extends React.Component {
         out = `${ prefixes[getRandomInt(0, prefixes.length - 1)] } ${ name }`;
         break;
 
+      // name nickname prefix type
+      case 3:
+        out = `${ name } ${ nicknames[getRandomInt(0, nicknames.length - 1)] }, ${ prefixes[getRandomInt(0, prefixes.length - 1)] } ${ types[getRandomInt(0, types.length - 1)] }`;
+        break;
+
       // name, type suffix
       case 4:
         out = `${ name }, ${ types[getRandomInt(0, types.length - 1)] } ${ suffixes[getRandomInt(0, suffixes.length - 1)] } `;
@@ -33,7 +41,7 @@ export default class NameGenerator extends React.Component {
 
       // name suffix
       case 5:
-        out = ` ${ name } ${ suffixes[getRandomInt(0, suffixes.length - 1)] }`;
+        out = `${ name } ${ suffixes[getRandomInt(0, suffixes.length - 1)] }`;
         break;
 
       // name nickname suffix
@@ -51,20 +59,21 @@ export default class NameGenerator extends React.Component {
         out = `${ prefixes[getRandomInt(0, prefixes.length - 1)] } ${ name } ${ nicknames[getRandomInt(0, nicknames.length - 1)] }`;
         break;
 
-      // name nickname prefix type
-      case 3:
-        out = `${ name } ${ nicknames[getRandomInt(0, nicknames.length - 1)] } ${ prefixes[getRandomInt(0, prefixes.length - 1)] } ${ types[getRandomInt(0, types.length - 1)] }`;
-        break;
-
-      // name nickname type
-      case 3:
-        out = `${ name }, ${ nicknames[getRandomInt(0, nicknames.length - 1)] } ${ types[getRandomInt(0, types.length - 1)] }`;
-        break;
-
       // prefix name, type suffix
       case 9:
         out = `${ prefixes[getRandomInt(0, prefixes.length - 1)] } ${ name }, ${ types[getRandomInt(0, types.length - 1)] } ${ suffixes[getRandomInt(0, suffixes.length - 1)] }`;
         break;
+
+      // name nickname type
+      case 10:
+        out = `${ name }, ${ nicknames[getRandomInt(0, nicknames.length - 1)] } ${ types[getRandomInt(0, types.length - 1)] }`;
+        break;
+
+      // name type
+      case 11:
+        out = `${ types[getRandomInt(0, types.length - 1)] } ${ name }`;
+        break;
+      /* eslint-enable max-len */
     }
 
     this.setState({ bossName: out });
